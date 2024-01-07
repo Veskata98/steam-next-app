834b73ee9f856eedbb5e6b6e207078d5c827a3e3
Veskata98 <vescko332@gmail.com>
2024-01-07 13:22:05 +0200

    Added shadcn ui components

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from '@/components/navbar/Navbar';
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between">
+        <main className="flex min-h-screen w-full justify-between">
             <Navbar />
         </main>
     );
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,5 +1,12 @@
-const Navbar = () => {
-    return <div></div>;
-};
+'use client';
 
-export default Navbar;
+import { Button } from '../ui/button';
+
+export default function Navbar() {
+    return (
+        <nav className="flex justify-between w-5/6 mx-auto p-10">
+            <h2 className="text-3xl font-bold">Steam Crawler</h2>
+            <Button>Refresh</Button>
+        </nav>
+    );
+}
